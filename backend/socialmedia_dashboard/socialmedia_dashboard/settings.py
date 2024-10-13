ada672878eeef7a5902aa57b2c9ec7f82225fa73
dev-ahmed-hatem <ahmedhatemezzathelal@gmail.com>
2024-10-13 23:35:39 +0300

    added requirements.txt file

--- backend/socialmedia_dashboard/socialmedia_dashboard/settings.py
+++ backend/socialmedia_dashboard/socialmedia_dashboard/settings.py
@@ -110,6 +110,7 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = join(BASE_DIR, "media")
+STATIC_ROOT = join(BASE_DIR, "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
